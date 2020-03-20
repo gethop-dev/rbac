77f9beed8c8e07b56ce4fb914173eb197fd6f574
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-03-20 16:41:35 +0100

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,8 @@
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :min-lein-version "2.9.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.cache "0.7.1"]
-                 [metosin/spec-tools "0.9.2"]
-                 [magnet/sql-utils "0.4.7"]
+                 [org.clojure/core.cache "0.8.2"]
+                 [magnet/sql-utils "0.4.8"]
                  [org.postgresql/postgresql "42.2.9"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
@@ -22,5 +21,6 @@
    :repl {:repl-options {:host "0.0.0.0"
                          :port 4001}}
    :profiles/dev {}
-   :project/dev {:plugins [[jonase/eastwood "0.3.7"]
+   :project/dev {:dependencies [[duct/logger "0.3.0"]]
+                 :plugins [[jonase/eastwood "0.3.10"]
                            [lein-cljfmt "0.6.6"]]}})
